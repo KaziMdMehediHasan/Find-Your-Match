068dfb4a85b80bec8de992bb9a783d023731554e
mehedee92 <49508551+mehedee92@users.noreply.github.com>
2019-09-23 21:30:08 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+
+var randomNumber = Math.floor(Math.random() * 10) + 1;
+
+var celeb = "images/find" + randomNumber + ".png";
+
+document.querySelector("img").setAttribute("src" , celeb);
+
+///
+
+if(celeb = "find1"){
+  document.querySelector("h1").innerHTML = "Volia ! We found your Betterhalf !";
+}
